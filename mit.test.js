38d4382b6775dfd8068ebc14ed0bfa4b8d4f547f
Gabriel Grinstein <gabriel.grinstein@gmail.com>
2022-03-22 15:47:00 -0700

    fixed mit object

--- mit.test.js
+++ mit.test.js
@@ -1,28 +1,30 @@
-var utils  = require('course-utilities');
-var mit = utils.load('./mit.js', 'mit');
+// Jest Testing Framework
 
-describe('MIT', () => {
+var utils = require("course-utilities");
+var mit = utils.load("./mit.js", "mit");
 
-    test('Object properties present', () => {
-        expect(mit).toHaveProperty('city');
-        expect(mit).toHaveProperty('colors');
-        expect(mit).toHaveProperty('mascot');
-    });
+// Test Suite name: MIT (declared line below)
+describe("MIT", () => {
+  test("Object properties present", () => {
+    expect(mit).toHaveProperty("city");
+    expect(mit).toHaveProperty("colors");
+    expect(mit).toHaveProperty("mascot");
+  });
 
-    test('City match', () => {
-        expect(mit.city).toBe('Cambridge');
-    });
+  test("City match", () => {
+    expect(mit.city).toBe("Cambridge");
+  });
 
-    test('Colors match', () => {
-        expect(mit.colors).toContain('Silver Gray');    
-    });
+  test("Colors match", () => {
+    expect(mit.colors).toContain("Silver Gray");
+  });
 
-    test('Founded range', () => {
-        expect(mit.founded).toBeLessThanOrEqual(1875);
-        expect(mit.founded).toBeGreaterThanOrEqual(1850);    
-    });
+  test("Founded range", () => {
+    expect(mit.founded).toBeLessThanOrEqual(1875);
+    expect(mit.founded).toBeGreaterThanOrEqual(1850);
+  });
 
-    test('Motto', () => {
-        expect(mit.motto).toMatch('Mens');
-    });
+  test("Motto", () => {
+    expect(mit.motto).toMatch("Mens");
+  });
 });
